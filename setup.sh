d0671b86919dbe4f782e44686d215d5720770d7d
Oliver Butler <olly@protoncloud.co.uk>
2019-08-11 14:35:12 +0100

    Update script to autoinstall

--- setup.sh
+++ setup.sh
@@ -1,30 +1,38 @@
 #!/bin/sh
 
+# Set colors to variables
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+GREEN='\033[0;32m'
+WHITE='\033[1;37m'
+
 dependencies=(
     "sl" "trash" "screenfetch"
 )
 
 if [ $1 ]; then
     if [ $1 = "-u" ]; then
-        echo "[Updating] $OSTYPE...\n"
+        echo "${WHITE}[${PURPLE}Updating${WHITE}] $OSTYPE"
         if [ "$OSTYPE" = "darwin18" ]; then
             /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
             for dep in ${dependencies[*]}; do
-                echo "  [Installing] => " $dep
+                echo "  ${WHITE}[${PURPLE}Installing${WHITE}] => " $dep
                 brew install $dep
+                echo "  ${WHITE}[${GREEN}DONE${WHITE}] Installed $dep successfully"
             done
         else
             sudo apt-get update && sudo apt-get upgrade
             for dep in ${dependencies[*]}; do
-                echo "  [Installing] => " $dep
+                echo "  ${WHITE}[${PURPLE}Installing${WHITE}] => " $dep
                 apt install $dep
+                echo "  ${WHITE}[${GREEN}DONE${WHITE}] Installed $dep successfully"
             done
         fi
-    elif [ $1 = "-b" ]; then
-        echo "[Backup] /etc/profile"
-        sudo cp /etc/profile /etc/profile.bck
     fi
 fi
-echo "[Propagating] bash_aliases to /etc/profile"
-sudo cp $HOME/projects/linux-custom/.bash_aliases /etc/profile
-echo "====> Please run source '/etc/profile' or restart shell <===="
+echo "${WHITE}[${PURPLE}Propagating${WHITE}] bash_aliases to home directory"
+sudo cp $HOME/projects/linux-custom/.bash_aliases ~/.bash_profile
+echo "  ${WHITE}[${GREEN}DONE${WHITE}] cp >> ~/.bash_profile"
+sudo cp $HOME/projects/linux-custom/.bash_aliases ~/.bashrc
+echo "  ${WHITE}[${GREEN}DONE${WHITE}] cp >> ~/.bashrc"
+echo "${WHITE}[${YELLOW}WARN${WHITE}] Run . ~/.bash_profile or restart shell"
